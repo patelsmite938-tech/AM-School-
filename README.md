4784b2dca8b26f27802dfb7868a31ec72b9339ab
patelsmite938-tech <patelsmite938@gmail.com>
2026-01-05 11:18:52 +0530

    first  commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AM-School-
+
+   git config --global user.email "patelsmite938@gmail.com"
+  git config --global user.name "patelsmite938-tech"
---
+++ srms/index.php
@@ -0,0 +1,76 @@
+<?php
+error_reporting(0);
+include('includes/config.php'); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>AM School Management System</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="index.php"><b><i><u>AM-( School Management System )</u></i></b></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!"><b><i><u>Home</b></i></u></a></li>
+                        <li class="nav-item"><a class="nav-link active" href="find-result.php"><b><i><u>Students</b></i></u></a></li>
+                        <li class="nav-item"><a class="nav-link active" href="admin-login.php"><b><i><u>Admin</b></i></u></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header - set the background image for the header in the line below-->
+    
+
+        <img class="d-block w-100" src="images/1.png" alt="not" height=500>
+        <!-- Content section-->
+        <section class="py-5">
+            <div class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <h2><b><i><u>Notice Board</b></i></u></h2>
+                        <hr color="#000" />
+                        <marquee direction="up"  onmouseover="this.stop();" onmouseout="this.start();">
+                   <ul>
+ <?php $sql = "SELECT * from tblnotice";
+$query = $dbh->prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{   ?>                      
+<li><a href="notice-details.php?nid=<?php echo htmlentities($result->id);?>" target="_blank"><?php echo htmlentities($result->noticeTitle);?></li>
+<?php }} ?>
+
+                   </ul>
+               </marquee>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white"><b><i><u>Copyright &copy; AM-School Management System </b></i></u><?php echo date('Y');?></p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
